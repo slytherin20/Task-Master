5f08cb2ad9bcd2fe4ca9f2499ac199cb4ad1a5c7
slytherin20 <sonaligupta2904@gmail.com>
2021-05-29 20:13:12 +0530

    Feat - Displayed time in correct order

--- src/displaytasks.js
+++ src/displaytasks.js
@@ -7,7 +7,9 @@ function Display({tasks}){
                         <h3>{task.taskName}</h3>
                         <button style={{border:`solid 2px ${task.priority==="High"?"red":(task.priority==="Medium"?"yellow":"green")}`}}>{task.priority}</button>
                         <p>{task.status}</p>
-                        <p>{task.deadline}</p>
+                        <p>{
+                            task.deadline.split("-").reverse().join("-")
+                            }</p>
                         <button className="done">Done</button>
                         <button className="delete">Delete</button>
                         </div>
