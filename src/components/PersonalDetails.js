5123110f9e16e4ba95985054ffc6e611242f9889
slytherin20 <sonaligupta2904@gmail.com>
2021-06-15 15:00:15 +0530

    BUG FIXED - Duplicate name removed

--- src/components/PersonalDetails.js
+++ src/components/PersonalDetails.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import firebase from "firebase/app";
 import noPhoto from '../images/nophoto.jpg';
 import closeBtn from "../images/cancel.png";
@@ -12,10 +12,32 @@ export default function PersonalDetails({userId}){
     const [imageName,setImageName] = useState("nophoto.jpg");
     const [imageText,setImageText] = useState("");
     const [imageSrc,setImageSrc] = useState(noPhoto);
+    const [storedName,setStoredName] = useState({});
+    const [storedImage,setStoredImage] = useState(null);
 
     //Collection ref
     const userID = auth.currentUser.uid;
     const collectionRef = db.collection(`users/${userID}/name`);
+
+    useEffect(() => {
+        //Get the name from firestore
+        collectionRef.onSnapshot(function (querySnapshot){
+                querySnapshot.docs.map((doc)=>{
+                    setStoredName({
+                        id: doc.id,
+                        first: doc.data().first,
+                        last: doc.data().last
+                    }
+                    )
+                })
+        },
+        (error)=>{
+            console.log("Name does not exist:",error)
+        }
+        )
+    }, [])
+
+
 
     //User info
     const metadata = auth.currentUser.metadata;
@@ -26,24 +48,46 @@ export default function PersonalDetails({userId}){
         name==="first"?setFirstName(e.target.value)
                         :setLastName(e.target.value)
     }
+
     function submitDetails(e){
         e.preventDefault()
         //Store image
         if(imageName!=="nophoto.jpg"){
-            const storageRef = firebase.storage().ref(`users/${userId}/picture/`+imageName)
+            const storageRef = firebase
+                                .storage()
+                                .ref(`users/${userId}/picture/`+imageName)
             storageRef.put(image);
         }
         //Store name
+        addName()
+       
+    }
+    function addName(){
+
         if(firstName && lastName){
-            collectionRef.add({
-                first: firstName,
-                last: lastName
-            })
+
+            //If name not already exist.
+            if(Object.keys(storedName).length===0){
+                collectionRef.add({
+                    first: firstName,
+                    last: lastName
+                })
+            }
+            //If name already exist in database.
+            else{
+                collectionRef
+                .doc(storedName.id)
+                .update({
+                    first: firstName,
+                    last: lastName,
+                })
+            }
         }
     }
 
     function uploadImage(e){
-        if(e.target.files){
+
+        if(e.target.files[0]){
 
             let file = e.target.files[0];
             let fileName = file.name;
@@ -76,7 +120,7 @@ export default function PersonalDetails({userId}){
                             <img 
                                 src={closeBtn} 
                                 alt="close-tab"
-                                onClick={console.log("Close tab")}>
+                                onClick={()=>console.log("Close tab")}>
                             </img>
                         </button>
                     }
