6cdc64edbe7096ffdbc794081f7a3de3c4601ede
slytherin20 <sonaligupta2904@gmail.com>
2021-09-07 17:33:07 +0530

    FEAT - added loading page on app start

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,13 +2,28 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import App from "./app";
 import SignUp from "./SignUp";
 import { auth } from "../utilities/functions/firebase_config";
+import Loader from "react-loader-spinner";
 
 function Main(){
-    const [user] = useAuthState(auth);
+    const [user,loading,error] = useAuthState(auth);
 
-    return user?
-            <App />
-            :<SignUp />
+    if(loading){
+        return <div className="main-screen">
+            <div className="loader-container">
+            <Loader
+                type="TailSpin"
+                color="rgb(45, 59, 255)"
+                height={100}
+                width={100}/>
+            </div>
+        </div>
+                
+    }
+    if(error){
+        <h1>Woops! An error has occured. Please try refreshing the page.</h1>
+    }
+
+   return  user?<App />: <SignUp />
 }
 
 export default Main;
